7c77f769136b2c6709c7a5f5a9a1754e1c87f2e3
Kylash Krishnamurthy <kylashk@users.noreply.github.com>
2019-09-29 22:06:39 -0400

    Revert "Merge branch 'master' of https://github.com/kylashkrishnamurthy/twistter"
    This reverts commit 6f3122d42b0e1cd65aa022828ccdd997da518a1d, reversing
    changes made to 1e6f3b6ab3e2436e18133c57554d07f864206354.

--- server/app.py
+++ server/app.py
@@ -2,13 +2,12 @@ from flask import Flask, json, request, render_template, redirect
 from flask_cors import CORS
 
 
-app = Flask(__name__, template_folder = "../client/public")
+app = Flask(__name__)
 CORS(app)
 
-@app.route('/')
-@app.route('/login')
-def login():
-    return render_template("index.html")
+@app.route('/index')
+def hello():
+    return "Hello World"
 
 
 if __name__ == "__main__":
