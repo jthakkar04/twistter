54ee89e6cc414c860b43015cc68a0e450f24e31f
Puja Maheshwari <pujam1231@gmail.com>
2019-10-31 15:41:51 -0400

    microblog in the works

--- client/src/components/homePage/feedPage.jsx
+++ client/src/components/homePage/feedPage.jsx
@@ -1,18 +1,38 @@
 // Dependencies
 import React from 'react';
+import microBlog  from './microblog';
 
 export class FeedPage extends React.Component {
 
-    // Set state so it can be redirected if not logged in
-    state = {
-        redirectToReferrer: false
+    constructor(props){
+        super(props)
+        this.state = {
+            redirectToReferrer: false,
+        }
     }
 
+    // Set state so it can be redirected if not logged in
+    // state = {
+    //     redirectToReferrer: false
+    // }
+
     render() {
+        const data = ['one', 'two', 'three']
+
 
         return (
             <div>
                 <h1>Home Page</h1>
+                <div>
+                    < microBlog data={data} />
+                    <ul class="flex-container column">
+                        <li class="flex-item">1</li>
+                        <li class="flex-item">2</li>
+                        <li class="flex-item">3</li>
+                        <li class="flex-item">4</li>
+                        <li class="flex-item">5</li>
+                    </ul>
+                </div>
             </div>
 
         );
