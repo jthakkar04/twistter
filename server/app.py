9d3eca97e51fce05fd09e9646d4c163eedba8715
Prahlad Anand <anprahlad@pal-nat186-13-156.itap.purdue.edu>
2019-11-17 11:55:57 -0500

    edited userID insert

--- server/app.py
+++ server/app.py
@@ -94,6 +94,7 @@ def put_user():
     userInfo=request.json
     username=userInfo['username']
     email=userInfo['email']
+    userId = userInfo['user_id']
     firstName=userInfo['first_name']
     lastName=userInfo['last_name']
     numFollowers=0
@@ -102,9 +103,9 @@ def put_user():
     verified=0
     bio=userInfo['bio']
 
-    query = "INSERT INTO users (username, email, first_name, last_name, num_followers, num_following, profile_pic, verified, bio) VALUES (%s, %s, %s, %s , %s, %s, %s, %s, %s)"
+    query = "INSERT INTO users (username, email, first_name, last_name, num_followers, num_following, profile_pic, verified, bio, user_id) VALUES (%s, %s, %s, %s , %s, %s, %s, %s, %s,%s)"
 
-    vals=(username, email, firstName, lastName, numFollowers, numFollowing, profilePic, verified, bio,)
+    vals=(username, email, firstName, lastName, numFollowers, numFollowing, profilePic, verified, bio, userId,)
 
     cursor.execute(query, vals)
     cnx.commit()
