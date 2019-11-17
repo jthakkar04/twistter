c9a5006f5c46f93aed83ffffd36e7aa8138dac1f
Prahlad Anand <anprahlad@pal-nat186-13-156.itap.purdue.edu>
2019-11-17 11:49:48 -0500

    add follower/following logic and change datatype of user in mysql

--- server/database/create_tables.py
+++ server/database/create_tables.py
@@ -11,10 +11,10 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-mycursor.execute("CREATE TABLE users (user_id INT AUTO_INCREMENT PRIMARY KEY, username VARCHAR(20), email VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), num_followers INT, num_following INT, profile_pic VARCHAR(255), verified BOOL, bio VARCHAR(255))")
-mycursor.execute("INSERT INTO users (username) VALUES ('yung_yuri')")
-mycursor.execute("CREATE TABLE topics (topic_id INT AUTO_INCREMENT PRIMARY KEY, topic_name VARCHAR (15))")
-mycursor.execute("CREATE TABLE microblogs (twist_id INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(240), timestamp VARCHAR (255), user_id int, FOREIGN KEY (user_id) REFERENCES users(user_id), link VARCHAR(255), is_reply BOOL)")
+#mycursor.execute("CREATE TABLE users (user_id VARCHAR(25) PRIMARY KEY, username VARCHAR(25), email VARCHAR(255), first_name VARCHAR(255), last_name VARCHAR(255), num_followers INT, num_following INT, profile_pic VARCHAR(255), verified BOOL, bio VARCHAR(255))")
+#mycursor.execute("CREATE TABLE follower_following (follower_id VARCHAR(25), FOREIGN KEY (follower_id) REFERENCES users(user_id), following_id VARCHAR(25), FOREIGN KEY (following_id) REFERENCES users(user_id))")
+#mycursor.execute("CREATE TABLE topics (topic_id INT AUTO_INCREMENT PRIMARY KEY, topic_name VARCHAR (15))")
+mycursor.execute("CREATE TABLE microblogs (twist_id INT AUTO_INCREMENT PRIMARY KEY, text VARCHAR(240), timestamp VARCHAR (255), user_id VARCHAR (25), FOREIGN KEY (user_id) REFERENCES users(user_id), link VARCHAR(255), is_reply BOOL)")
 
 #mycursor.execute("INSERT INTO microblogs (text, timestamp, user_id) VALUES ('i hate CS', '00:00:00', '4')")
 
